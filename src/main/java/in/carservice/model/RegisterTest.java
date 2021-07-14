fdd68b432453d90cd190f5e383ef590d08d2f417
Deepa0811 <deepabalasubramani333@gmail.com>
2021-07-14 16:05:45 +0530

    model,test-deepa

---
+++ src/main/java/in/carservice/model/RegisterTest.java
@@ -0,0 +1,17 @@
+package in.yourproject;
+
+public class RegisterTest {
+	public static void main(String[] args) {
+
+		Register register1 = new Register("abcd","efgh","abcd","a@gmail.com","1234");
+			
+			System.out.println(register1.toString());
+           Register register2 = new Register("ijkl","mnop","ijkl","b@gmail.com","5678");
+			
+			System.out.println(register2.toString());
+
+			
+					}
+
+	}
+
